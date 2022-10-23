24eb0c0f37725aaffe9f4d1c018691a39a603c4f
Patrick Leiser <patrick27leiser@yahoo.com>
2022-10-23 00:57:41 -0700

    update, now a proper script
    now handles a duration argument, or -h, or no argument for default real-time behavior

--- watchTimeMachineLog.sh
+++ watchTimeMachineLog.sh
@@ -1,10 +1,27 @@
 #for watching the logs from an Apple Time Machine backup on MacOS
 #the log will update with new progress every 5 minutes
-log stream --predicate 'subsystem == "com.apple.TimeMachine"' --info
+#or get historical records by specifying a durarion of time to look back
 
-#to see the recent historical records, instead run
-#log show --predicate 'subsystem == "com.apple.TimeMachine"' --info --last 4h
-#                                         can adjust duration to show here ^
+RED="$(tput setaf 1)"
+YELLOW="$(tput setaf 3)"
+CYAN="$(tput setaf 6)"
+WHITE="$(tput setaf 7)"
+
+
+
+if (( $# == 0 )); then
+	log stream --predicate 'subsystem == "com.apple.TimeMachine"' --info
+elif [[ $1 == *-h* ]]; then
+	echo "usage: ${CYAN}$0 [duration]${WHITE}"
+	echo "duration specifier such as ${CYAN}20m${WHITE} or ${CYAN}4h${WHITE}"
+	echo "or ${CYAN}leave duration blank${WHITE} for live data stream"
+else
+
+#to see the recent historical records, pass a time specifier
+
+log show --predicate 'subsystem == "com.apple.TimeMachine"' --info --last $1
+#               choose argument such as 4h to adjust duration to show here ^
+fi
 
 #to hide non-progress status messages, adjust the --predicate string to
 #--predicate '(subsystem == "com.apple.TimeMachine") && (category == "CopyProgress")'
